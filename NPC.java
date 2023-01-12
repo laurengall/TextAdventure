d5fecd963db3cf731f0fb42ae16090556e36e305
Lauren Gallagher <109532911+laurengall@users.noreply.github.com>
2023-01-12 12:23:46 -0500

    Update NPC.java

--- NPC.java
+++ NPC.java
@@ -24,16 +24,46 @@ public void setIsActive(boolean isActive){
   }
 
 
-public void chase(){
+public void chase(int score){
 isActive = true; 
-System.out.println("You have all of the evidence you need! But, alas, the suspect has discovered you are in the house! Get back to the Entry Way in less that 20 moves."); 
   
-System.out.println(npcName + " is in pursuit!"); 
+  
+  System.out.println("Your current score is "+ score + "/330"); 
+  
+  System.out.println("Press 1 to keep playing, press 2 to quit the game."); 
+  
+String input = Input.getInput();
+  
+boolean keepAsking = true; 
+  
+  while (keepAsking == true) {
+    if (input.equals("1")){
+    System.out.println("You need eleven more items. Please continue playing. You can quit the game at any time by typing 'quit'. You will still win the game."); 
+      keepAsking = false; 
+  } 
+    else if (input.equals("2")){
+    System.out.println("YOU WIN. TOTAL SCORE: " + score + "/330"); 
+      keepAsking = false; 
+        System.exit(0);
+  }
+    else {
+   System.out.println("Please type 1 to resume the game or 2 to quit the game."); 
+      keepAsking = true; 
+      input = Input.getInput();
+  }
+  
+  
+  
+  }
+          
+   
+ }
+ 
 
-}
-
+ 
+ }
     
-    }
+  
   
 
 
